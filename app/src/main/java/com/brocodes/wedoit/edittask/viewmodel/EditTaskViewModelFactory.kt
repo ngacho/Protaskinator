915390618c3356efa28361f1568860d365d001b5
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-22 23:51:27 +0300

    feature added: edit task viewmodel set up

--- app/src/main/java/com/brocodes/wedoit/edittask/viewmodel/EditTaskViewModelFactory.kt
+++ app/src/main/java/com/brocodes/wedoit/edittask/viewmodel/EditTaskViewModelFactory.kt
@@ -1,4 +1,14 @@
 package com.brocodes.wedoit.edittask.viewmodel
 
-class EditTaskViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.brocodes.wedoit.model.repository.TaskRepository
+
+class EditTaskViewModelFactory constructor(private val repository: TaskRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return EditTaskViewModel(repository) as T
+    }
 }
