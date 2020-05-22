91fd93263385eccac533704c63b498aaa9726022
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-22 23:40:12 +0300

    feature added: adding task to sqllite

--- app/src/main/java/com/brocodes/wedoit/addtask/AddTaskFragment.kt
+++ app/src/main/java/com/brocodes/wedoit/addtask/AddTaskFragment.kt
@@ -1,14 +1,22 @@
 package com.brocodes.wedoit.addtask
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.brocodes.wedoit.R
+import com.brocodes.wedoit.addtask.viewmodel.AddTaskViewModel
+import com.brocodes.wedoit.addtask.viewmodel.AddTaskViewModelFactory
 import com.brocodes.wedoit.databinding.FragmentAddTaskBinding
+import com.brocodes.wedoit.mainactivity.MainActivity
+import com.brocodes.wedoit.model.entity.Task
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -22,6 +30,13 @@ class AddTaskFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val repository = (activity as MainActivity).taskRepository
+
+        val addTaskViewModel = ViewModelProvider(
+            this,
+            AddTaskViewModelFactory(repository)
+        ).get(AddTaskViewModel::class.java)
 
         // Set dialog initial state when shown
         dialog?.setOnShowListener {
@@ -44,19 +59,41 @@ class AddTaskFragment : BottomSheetDialogFragment() {
         val priorityPicker = addTaskBinding.numberPicker
         val saveButton = addTaskBinding.saveButton
         val cancelButton = addTaskBinding.cancelButton
+        val datePicker = addTaskBinding.dateDuePicker
+
 
         saveButton.setOnClickListener {
-
-            Toast.makeText(this.context, "Task Saved", Toast.LENGTH_SHORT).show()
-            Log.i("Save Button", "Task Saved! Priority: ${priorityPicker.value}")
-            dismiss()
+            //Fetch task if both task title and task name are not the same
+            if (!taskNameEditText.text.toString()
+                    .isBlank() && !taskDescriptionEditText.text.toString().isBlank()
+            ) {
+                val taskName = taskNameEditText.text.toString().trim()
+                val taskDescription = taskDescriptionEditText.text.toString().trim()
+                val priority = priorityPicker.value
+                val date = "${datePicker.dayOfMonth + 7}-${datePicker.month}-${datePicker.year}"
+                val task = Task(
+                    taskTitle = taskName,
+                    taskDescription = taskDescription,
+                    priority = priority,
+                    date = date
+                )
+                addTaskViewModel.addTask(task)
+                Toast.makeText(this.context, "Task Saved", Toast.LENGTH_SHORT).show()
+                dismiss()
+            } else if (taskNameEditText.text.toString().isBlank()) {
+                //Error is task name is empty
+                taskNameEditText.requestFocus()
+                Toast.makeText(this.context, "Please Name your task", Toast.LENGTH_SHORT).show()
+            } else if (taskDescriptionEditText.text.toString().isBlank()) {
+                //error if task description is empty
+                taskNameEditText.requestFocus()
+                Toast.makeText(this.context, "Please Describe your task", Toast.LENGTH_SHORT).show()
+            }
         }
 
         cancelButton.setOnClickListener {
-            Log.i("Cancel Button", "Save Operation Cancelled")
             dismiss()
         }
-
 
         return addTaskBinding.root
     }
--- app/src/main/java/com/brocodes/wedoit/addtask/viewmodel/AddTaskViewModel.kt
+++ app/src/main/java/com/brocodes/wedoit/addtask/viewmodel/AddTaskViewModel.kt
@@ -1,4 +1,13 @@
 package com.brocodes.wedoit.addtask.viewmodel
 
-class AddTaskViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.brocodes.wedoit.model.entity.Task
+import com.brocodes.wedoit.model.repository.TaskRepository
+import kotlinx.coroutines.launch
+
+class AddTaskViewModel(private val repository: TaskRepository) : ViewModel(){
+    fun addTask(task : Task) = viewModelScope.launch {
+        repository.insertTask(task)
+    }
 }
