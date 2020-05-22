c8fdc19ff7c64833bc3ea90de2b0091b514e1461
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-22 23:40:50 +0300

    feature added: adapters for viewing tasks

--- app/src/main/java/com/brocodes/wedoit/commonutils/BindingAdapters.kt
+++ app/src/main/java/com/brocodes/wedoit/commonutils/BindingAdapters.kt
@@ -1,2 +1,17 @@
 package com.brocodes.wedoit.commonutils
 
+import android.graphics.Color
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.brocodes.wedoit.R
+
+@BindingAdapter("priority")
+fun setTextViewColor(textView: TextView, int: Int) = when(int){
+    1 -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.priorityOne))
+    2 -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.priorityTwo))
+    3 -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.priorityThree))
+    4 -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.priorityFour))
+    5 -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.priorityFive))
+    else -> textView.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.fragmentsBackground))
+}
--- app/src/main/java/com/brocodes/wedoit/commonutils/TaskListAdapter.kt
+++ app/src/main/java/com/brocodes/wedoit/commonutils/TaskListAdapter.kt
@@ -1,4 +1,37 @@
 package com.brocodes.wedoit.commonutils
 
-class ListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.brocodes.wedoit.R
+import com.brocodes.wedoit.databinding.TaskListItemBinding
+import com.brocodes.wedoit.model.entity.Task
+
+class TaskListAdapter(private val taskList : List<Task>) : RecyclerView.Adapter<TaskListAdapter.TaskListItemViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val taskListItemBinding = DataBindingUtil.inflate<TaskListItemBinding>(
+            layoutInflater,
+            R.layout.task_list_item,
+            parent,
+            false
+        )
+
+        return TaskListItemViewHolder(taskListItemBinding)
+    }
+
+    override fun getItemCount(): Int = taskList.size
+
+    override fun onBindViewHolder(holder: TaskListItemViewHolder, position: Int) = holder.bind(taskList[position])
+
+    class TaskListItemViewHolder(private val taskListItem: TaskListItemBinding) : RecyclerView.ViewHolder(taskListItem.root) {
+        fun bind(task : Task){
+            taskListItem.task = task
+            taskListItem.executePendingBindings()
+        }
+
+    }
 }
