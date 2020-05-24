252dc65151f368f070ab6fea0682cb2643bec247
Ngacho <52356809+ngacho@users.noreply.github.com>
2020-05-25 01:38:48 +0300

    feature added: swipe gestures enabled

--- app/src/main/java/com/brocodes/wedoit/mytasks/MyTasksFragment.kt
+++ app/src/main/java/com/brocodes/wedoit/mytasks/MyTasksFragment.kt
@@ -1,11 +1,14 @@
 package com.brocodes.wedoit.mytasks
 
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -14,10 +17,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.brocodes.wedoit.R
-import com.brocodes.wedoit.commonutils.SwipeActionCallback
 import com.brocodes.wedoit.commonutils.TaskListAdapter
 import com.brocodes.wedoit.databinding.FragmentMyTasksBinding
 import com.brocodes.wedoit.mainactivity.MainActivity
+import com.brocodes.wedoit.model.entity.Task
 import com.brocodes.wedoit.mytasks.viewmodel.MyTasksViewModel
 import com.brocodes.wedoit.mytasks.viewmodel.MyTasksViewModelFactory
 
@@ -61,40 +64,99 @@ class MyTasksFragment : Fragment() {
             myTasksRecyclerView.adapter = taskListAdapter
         })
 
-        val swipeToDeleteCallback = object : SwipeActionCallback(
-            requireContext(),
+        val paint = Paint()
+        val swipeCallBack = object : ItemTouchHelper.SimpleCallback(
             0,
             ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
         ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 if (direction == ItemTouchHelper.RIGHT) {
-                    Log.d("Right swipe", "Item completed")
-                } else {
-                    Log.d("Task swiped", "Position $direction")
-                    val task = taskListAdapter.getTaskAt(viewHolder.adapterPosition)
-                    myTasksRecyclerView.adapter?.notifyItemRemoved(viewHolder.adapterPosition)
-                    myTasksViewModel.deleteTask(task)
-                }
 
+                } else {
+                    val deletedTask = taskListAdapter.getTaskAt(viewHolder.adapterPosition)
+                    myTasksViewModel.deleteTask(deletedTask)
+                    taskListAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+                }
             }
 
+            override fun onChildDrawOver(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+
+                val swipedItemView = viewHolder.itemView
+                val height = swipedItemView.height
+                val width = height / 3
+                val icon: Drawable
+
+                if (dX > 0) {
+                    //color when swiped
+                    paint.color =
+                        ContextCompat.getColor(requireContext(), R.color.completeTaskColor)
+                    val background = RectF(
+                        swipedItemView.left.toFloat(),
+                        swipedItemView.top.toFloat(),
+                        dX,
+                        swipedItemView.bottom.toFloat()
+                    )
+                    canvas.drawRect(background, paint)
+
+                    icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_complete)!!
+                    val iconBounds = Rect(
+                        swipedItemView.left + width,
+                        swipedItemView.top + width,
+                        swipedItemView.left + 2 * width,
+                        swipedItemView.bottom - width
+                    )
+                    icon.bounds = iconBounds
+                    icon.draw(canvas)
+
+
+                } else {
+                    paint.color = ContextCompat.getColor(requireContext(), R.color.deleteTaskColor)
+                    val background = RectF(
+                        swipedItemView.right.toFloat() + dX,
+                        swipedItemView.top.toFloat(),
+                        swipedItemView.right.toFloat(),
+                        swipedItemView.bottom.toFloat()
+                    )
+                    canvas.drawRect(background, paint)
+
+                    icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_delete)!!
+                    val iconBounds = Rect(
+                        swipedItemView.right - 2 * width,
+                        swipedItemView.top + width,
+                        swipedItemView.right - width,
+                        swipedItemView.bottom - width
+                    )
+                    icon.bounds = iconBounds
+                    icon.draw(canvas)
+                }
+
+                super.onChildDrawOver(
+                    canvas,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+            }
         }
 
-        //configure right swipe
-        swipeToDeleteCallback.rightBG =
-            ContextCompat.getColor(requireContext(), R.color.completeTaskColor)
-        swipeToDeleteCallback.rightLabel = "Completed"
-        swipeToDeleteCallback.rightIcon =
-            AppCompatResources.getDrawable(requireContext(), R.drawable.ic_complete)
-
-        //configure left swipe
-        swipeToDeleteCallback.leftBG =
-            ContextCompat.getColor(requireContext(), R.color.deleteTaskColor)
-        swipeToDeleteCallback.leftLabel = "Delete"
-        swipeToDeleteCallback.leftIcon =
-            AppCompatResources.getDrawable(requireContext(), R.drawable.ic_delete)
-
-        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        val itemTouchHelper = ItemTouchHelper(swipeCallBack)
         itemTouchHelper.attachToRecyclerView(myTasksRecyclerView)
 
 
